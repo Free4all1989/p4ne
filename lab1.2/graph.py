1fbd744d711f64ec99b022ceb112de6568e95792
on.kirichenko <on.kirichenko@jet.su>
2021-04-21 15:32:29 +0300

    lab1.2 added

--- lab1.2/graph.py
+++ lab1.2/graph.py
@@ -8,15 +8,14 @@ shA = sheet['A'][1:]
 shC = sheet['C'][1:]
 shD = sheet['D'][1:]
 
+
 def getvalue(x):
     return x.value
 
-mshA = map(getvalue, shA)
-mshC = map(getvalue, shC)
-mshD = map(getvalue, shD)
-list_y = list(mshA)
-list_x1 = list(mshC)
-list_x2 = list(mshD)
+
+list_y = list(map(getvalue, shA))
+list_x1 = list(map(getvalue, shC))
+list_x2 = list(map(getvalue, shD))
 pyplot.plot(list_y, list_x1, label="Temp")
 pyplot.plot(list_y, list_x2, label="Active")
 pyplot.show()
